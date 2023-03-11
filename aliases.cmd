012801d80c9ab094a651198614b2af4c16c72fa7
BlueSourBoy <me@josefaguilar.com>
2023-03-11 16:54:56 -0600

    feat: changed shortcuts

--- aliases.cmd
+++ aliases.cmd
@@ -6,12 +6,27 @@
 
 :: Add to path by command
 
+DOSKEY test=echo "bruh"
+
 DOSKEY add_python26=set PATH=%PATH%;"C:\Python26\"
 DOSKEY add_python33=set PATH=%PATH%;"C:\Python33\"
 
+:: Reload this file
+DOSKEY .s= %USERPROFILE%\Programming\command_aliases\aliases.cmd
+
+:: Movement
+
+DOSKEY ..=cd ../
+DOSKEY ...=cd ../../
+DOSKEY ....=cd ../../../
+DOSKEY gohome=cd %USERPROFILE%\
+DOSKEY godown=cd %USERPROFILE%\Downloads\
+DOSKEY gopro=cd %USERPROFILE%\Programming\
+
 :: Commands
 
-DOSKEY ls=dir /B $*
+DOSKEY ls=dir \B $*
+DOSKEY ll=dir $*
 DOSKEY sublime=sublime_text $*  
     ::sublime_text.exe is name of the executable. By adding a temporary entry to system path, we don't have to write the whole directory anymore.
 DOSKEY gsp="C:\Program Files (x86)\Sketchpad5\GSP505en.exe"
@@ -21,8 +36,16 @@ DOSKEY alias=notepad %USERPROFILE%\Dropbox\alias.cmd
 DOSKEY gs=git status
 DOSKEY gad=git add $*
 DOSKEY gds=git diff --staged
+DOSKEY gus=git restore --staged *
+DOSKEY gap=git add -p
+
+:: NPM Run
+DOSKEY npmd=npm run dev 
+DOSKEY npmb=npm run build
 
 :: Common directories
 
 DOSKEY dropbox=cd "%USERPROFILE%\Dropbox\$*"
 DOSKEY research=cd %USERPROFILE%\Dropbox\Research\
+
+echo "Configurations loaded"
